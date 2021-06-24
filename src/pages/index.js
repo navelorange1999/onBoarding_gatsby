29d5a73e1da075a80451fe17d9629b72702dcd10
q.chen <q.chen@qchendeMacBook-Pro.local>
2021-06-24 15:11:11 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
 import Layout from "../components/layout/layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This is Home</p>
+      <StaticImage alt="gatsby" src="../images/icon.png"/>
     </Layout>
   )
 }
